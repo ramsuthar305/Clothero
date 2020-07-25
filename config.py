5a15aa84a6999fc511f341f262a7077af250f03c
ramsuthar305 <ramsuthar305.com>
2020-07-25 22:10:50 +0530

    initial commit

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from os.path import join, dirname, realpath
+
+class Config:
+    DEBUG = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SESSION_TYPE = os.environ.get('SESSION_TYPE')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    
