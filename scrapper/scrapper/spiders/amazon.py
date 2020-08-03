c866d81ea94813ef49d1877f480bc056bdce9525
ramsuthar305 <ramsuthar305.com>
2020-08-04 01:33:43 +0530

    final commit

--- scrapper/scrapper/spiders/amazon.py
+++ scrapper/scrapper/spiders/amazon.py
@@ -12,23 +12,23 @@ class QuotesSpider(scrapy.Spider):
 
     def start_requests(self):
 
-        urls = {
+        # urls = {
 
-            {"type": "jeans", "link": 'https://www.amazon.in/T-Shirts-Polos-Men/s?rh=n%3A1968120031&page='},
+        #     {"type": "jeans", "link": 'https://www.amazon.in/T-Shirts-Polos-Men/s?rh=n%3A1968120031&page='},
 
-            {"type": "tshirt", "link": "https://www.amazon.in/s/ref=mega_sv_s23_2_1_1_5?rh=i%3Aapparel%2Cn%3A1571271031%2Cn%3A!1571272031%2Cn%3A1968024031%2Cn%3A1968076031&ie=UTF8&lo=apparel"},
+        #     {"type": "tshirt", "link": "https://www.amazon.in/s/ref=mega_sv_s23_2_1_1_5?rh=i%3Aapparel%2Cn%3A1571271031%2Cn%3A!1571272031%2Cn%3A1968024031%2Cn%3A1968076031&ie=UTF8&lo=apparel"},
 
-            {"type": "shoes", "link": "https://www.amazon.in/b?node=1983519031&ref=mega_sv_s23_2_2_1_2"},
+        #     {"type": "shoes", "link": "https://www.amazon.in/b?node=1983519031&ref=mega_sv_s23_2_2_1_2"},
 
-            {"type": "watch", "link": "https://www.amazon.in/s?i=watches&bbn=2563504031&rh=n%3A1350387031%2Cn%3A%211350388031%2Cn%3A2563504031%2Cp_n_material_browse%3A1480914031&lo=image&hidden-keywords=-sponsored&ref=mega_sv_s23_2_3_1_2"},
+        #     {"type": "watch", "link": "https://www.amazon.in/s?i=watches&bbn=2563504031&rh=n%3A1350387031%2Cn%3A%211350388031%2Cn%3A2563504031%2Cp_n_material_browse%3A1480914031&lo=image&hidden-keywords=-sponsored&ref=mega_sv_s23_2_3_1_2"},
 
-            {"type": "sunglass", "link": "https://www.amazon.in/s/ref=mega_sv_s23_2_3_3_2?rh=i%3Aapparel%2Cn%3A1968036031&ie=UTF8&lo=apparel"},
+        #     {"type": "sunglass", "link": "https://www.amazon.in/s/ref=mega_sv_s23_2_3_3_2?rh=i%3Aapparel%2Cn%3A1968036031&ie=UTF8&lo=apparel"},
 
-                }
+        #         }
 
-        }
+        # }
         urls = 'https://www.amazon.in/T-Shirts-Polos-Men/s?rh=n%3A1968120031&page='
-        for page in range(20):
+        for page in range(200):
             yield scrapy.Request(url=urls+str(page), callback=self.parse)
 
     def parse(self, response):
--- server/app.py
+++ server/app.py
@@ -6,6 +6,11 @@ app.config.from_object("config.Config")
 mongo = PyMongo(app)
 mongo=mongo.db
 from bson import ObjectId
+import numpy as np
+import os
+import math 
+from math import sqrt
+#import pandas as pd
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
@@ -13,6 +18,60 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
+class TensorVector():
+
+  def __init__(self):
+      pass
+
+  def cosineSim(self,a1,a2):
+    sum = 0
+    suma1 = 0
+    sumb1 = 0
+    for i,j in zip(a1, a2):
+        suma1 += i * i
+        sumb1 += j*j
+        sum += i*j
+    cosine_sim = sum / ((sqrt(suma1))*(sqrt(sumb1)))
+    return cosine_sim
+
+  def jaccard_similarity(self,list1, list2):
+    intersection = len(list(set(list1).intersection(list2)))
+    union = (len(list1) + len(list2)) - intersection
+    return float(intersection) / union
+
+  def average(self,x):
+    assert len(x) > 0
+    return float(sum(x)) / len(x)
+
+  def pearson_def(self,x, y):
+    assert len(x) == len(y)
+    n = len(x)
+    assert n > 0
+    avg_x = self.average(x)
+    avg_y = self.average(y)
+    diffprod = 0
+    xdiff2 = 0
+    ydiff2 = 0
+    for idx in range(n):
+        xdiff = x[idx] - avg_x
+        ydiff = y[idx] - avg_y
+        diffprod += xdiff * ydiff
+        xdiff2 += xdiff * xdiff
+        ydiff2 += ydiff * ydiff
+
+    return diffprod / math.sqrt(xdiff2 * ydiff2)
+
+  def mean(self,vector1,vector2):
+    cs=self.cosineSim(vector1,vector2)
+    #js=self.jaccard_similarity(vector1,vector2)
+    ps=self.pearson_def(vector1,vector2)
+    #print(cs,ps)
+    average=(cs+ps)/2
+    #print(average)
+    return average
+
+  def compare(self,vector1,vector2):
+    return self.mean(np.array(vector1),np.array(vector2))
 
 
 @app.errorhandler(404) 
@@ -40,12 +99,37 @@ def get_match():
     if request.method=='POST':
         req = request.get_data()
         req=json.loads(req)
-        print(req)
-        user_picks=[None]*len(req)
-        for id in range(len(req)):
-            user_picks[id]=mongo.picks.find_one({'_id':ObjectId(req[id])},{'Vector':1})
-        print(len(user_picks))
-        return render_template('match.html')
+        #print(req)
+        user_picks=[None]*len(req['elemIds'])
+        for id in range(len(req['elemIds'])):
+            user_picks[id]=mongo.picks.find_one({'_id':ObjectId(req['elemIds'][id])},{'Vector':1})
+        #print(user_picks)
+        all_clothes=list(mongo.data.find({'Type':req['type'].lower()},{'Vector':1,'Type':1}).limit(100))
+        #print(all_clothes[0]['Type'])
+        class_obj=TensorVector()
+        all_comparison_rows=[[None]*100]*len(req['elemIds'])
+        ##print(all_comparison_rows)
+        for i in range(len(user_picks)):
+            for j in range(len(all_clothes)):
+              
+                vector1=json.loads(user_picks[i]['Vector'])
+                #print("type of vector id",all_clothes[j]['_id'])
+                #print("type of vector id",all_clothes[j]['Vector'])
+                vector2=json.loads(all_clothes[j]['Vector'])
+                #print("type of vector 2",np.array(vector2))
+
+                all_comparison_rows[i][j]={'id':all_clothes[j]['_id'],'similarity':class_obj.compare(vector1,vector2)}
+            all_comparison_rows[i]=sorted(all_comparison_rows[i], key=lambda k: k['similarity'], reverse=True)
+            all_comparison_rows[i]=all_comparison_rows[i][:50]
+            all_comparison_rows[i]=set([sub['id'] for sub in all_comparison_rows[i]])
+            #print(len(all_comparison_rows[i]))
+        final_list=list(set.intersection(*all_comparison_rows))
+        #print(final_list)
+        #print(len(final_list))
+        response=[None]*len(final_list)
+        for id in range(len(final_list)):
+            response[id]=mongo.data.find_one({'_id':final_list[id]},{'Vector':0,'_id':0})
+        return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
